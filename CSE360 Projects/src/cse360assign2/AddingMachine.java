438dc9aa7349ba4dfe3bcf26877aebe539b54e51
Preston <Preston@Preston-PC>
2019-10-07 14:06:47 -0700

    Initial commit of project.

---
+++ CSE360 Projects/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+/**
+ * @author Preston McArthur
+ * Assignment 2
+ * CSE360
+ * <p>
+ * Class adds and subtracts numbers to a total.
+ * </p>
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor for AddingMachine.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total.
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds number to total.
+	 * @param value number being added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts number from total.
+	 * @param value number being subtracted
+	 */
+	public void subtract (int value) {
+	
+	}
+	
+	/**
+	 * Shows history of inputs.
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Clears the total.
+	 */
+	public void clear() {
+	
+	}
+}
