7be8c236d004b59cd30d7dabb568d5aeb2c1904d
Preston <Preston@Preston-PC>
2019-10-07 14:29:17 -0700

    Updated methods with code.

--- CSE360 Projects/src/cse360assign2/AddingMachine.java
+++ CSE360 Projects/src/cse360assign2/AddingMachine.java
@@ -11,12 +11,14 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String calculations;
 	
 	/**
 	 * Constructor for AddingMachine.
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		calculations = "0";
 	}
 	
 	/**
@@ -24,7 +26,7 @@ public class AddingMachine {
 	 * @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -32,7 +34,8 @@ public class AddingMachine {
 	 * @param value number being added
 	 */
 	public void add (int value) {
-		
+		total = total + value;
+		calculations = calculations + " + " + value;
 	}
 	
 	/**
@@ -40,20 +43,22 @@ public class AddingMachine {
 	 * @param value number being subtracted
 	 */
 	public void subtract (int value) {
-	
+		total = total - value;
+		calculations = calculations + " - " + value;
 	}
 	
 	/**
 	 * Shows history of inputs.
 	 */
 	public String toString () {
-		return "";
+		return calculations;
 	}
 	
 	/**
 	 * Clears the total.
 	 */
 	public void clear() {
-	
+		total = 0;
+		calculations = "0";
 	}
 }
